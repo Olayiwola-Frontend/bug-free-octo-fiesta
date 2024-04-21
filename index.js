1c2c0e7a5f72141c62859bb3ffc2c8a9ec5859e7
Sholar247 <167676449+Sholar247@users.noreply.github.com>
2024-04-21 09:48:50 +0100

    Index.html

--- index.js
+++ index.js
@@ -1,14 +1,254 @@
-let express = require('express');
-let app = express();
-let ejs = require('ejs');
-const haikus = require('./haikus.json');
-const port = process.env.PORT || 3000;
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio website </title>
+    <link rel="stylesheet" href="weather.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+  </head>
+  <body>
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
+    <div class="scrollToTop-btn">
+      <i class="fas fa-angle-up"></i>
+    </div>
 
-app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
-});
+    <header>
+      <a href="#" class="brand">Frontend-Web Developer</a>
+      <div class="menu-btn"></div>
+      <div class="navigation">
+        <a href="#main">Home</a>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#services">Services</a>
+        <a href="#work">Work</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </header>
 
-app.listen(port);
+    <section class="main" id="main">
+      <div class="content">
+        <h2><br><span>Olayiwola </span></h2>
+        <div class="animated-text">
+          <h3>Frontend-Web Developer</h3>
+          <h3>Web Developer</h3>
+        </div>
+        <a href="#" class="btn">View My Projects</a>
+        <div class="media-icons">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+      </div>
+    </section>
+
+    <section class="about" id="about">
+      <div class="title reveal">
+        <h2 class="section-title">About Me</h2>
+      </div>
+      <div class="content">
+        <div class="column col-left reveal">
+          <div class="img-card">
+<img src="images/portfolio.jpg" alt="">
+          </div>
+        </div>
+        <div class="column col-right reveal">
+          <h2 class="content-title">@ Olayiwola-Frontend-Web Developer</h2>
+          <p class="paragraph-text">
+              I am a Frontend-Web Developer  I create and implement visually appealing and interactive user interfaces for websites and web applications. Proficient in HTML, CSS, and JavaScript, translating  design mockups into functional, responsive, and cross-browser compatible web pages. I collaborate closely with designers and backend developers to ensure seamless integration of frontend components and enhance overall user experience. Additionally, I stay updated with evolving web technologies and best practices to deliver optimal solutions in line with project requirements and industry standards
+          </p>
+          <a href="#" class="btn">See More</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="skills" id="skills">
+      <div class="title reveal">
+        <h2 class="section-title">My Skills</h2>
+      </div>
+      <div class="content">
+        <div class="column col-left reveal">
+          <h2 class="content-title">My Skills and Experiences</h2>
+          <a href="#" class="btn">See More</a>
+        </div>
+        <div class="column col-right reveal">
+          <div class="bar">
+            <div class="info">
+              <span>HTML</span>
+              <span>95%</span>
+            </div>
+            <div class="line html"></div>
+          </div>
+          <div class="bar">
+            <div class="info">
+              <span>CSS</span>
+              <span>85%</span>
+            </div>
+            <div class="line css"></div>
+          </div>
+          <div class="bar">
+            <div class="info">
+              <span>Javascript</span>
+              <span>80%</span>
+            </div>
+            <div class="line javascript"></div>
+          </div>
+          <div class="bar">
+            <div class="info">
+              <span>JQuery</span>
+              <span>80%</span>
+            </div>
+            <div class="line jquery"></div>
+          </div>
+          <div class="bar">
+            <div class="info">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="services" id="services">
+      <div class="title reveal">
+        <h2 class="section-title">My Services</h2>
+      </div>
+      <div class="content">
+        <div class="card reveal">
+          <div class="service-icon">
+            <i class="fas fa-palette"></i>
+          </div>
+          <div class="info">
+            <h3>Frontend-Web</h3>
+            <p>I make 
+Use of  markup languages like HTML, CSS and JavaScript to bring concept to life and to  create user-friendly web pages
+Maintaining and improving  website
+Optimizing applications for maximum speed
+Design mobile-based features
+Collaborate with back-end developers and web designers to improve usability
+Get feedback from, and build solutions for, users and customers
+Write functional requirement documents and guides
+Create quality mockups and prototypes.
+            </p>
+          </div>
+        </div>
+        <div class="card reveal">
+          <div class="service-icon">
+            <i class="fas fa-file-code"></i>
+          </div>
+          <div class="info">
+            <h3>Website Development</h3>
+            <p>Design, build and maintain a web-based suite of game creation, testing and public release tools.
+Communicate with various project stakeholders across marketing, content, design, development, and finance to understand and execute vision under reasonable timeframes and budgets.
+Provide strategy and vision for innovative gameplay to improve user experience.
+ Maintained the current website and added more appealing features
+Redesigned sections of the website to ensure a more attractive and user friendly experience.</p>
+          </div>
+      </div>
+    </section>
+
+    <section class="work" id="work">
+      <div class="title reveal">
+        <h2 class="section-title">My Work</h2>
+      </div>
+      <div class="content">
+        <div class="card reveal">
+          <div class="card-img">
+            <img src="images/picture 1.jpg" alt="">
+          </div>
+        </div>
+        <div class="card reveal">
+          <div class="card-img">
+            <img src="images/picture 2.jpg" alt="">
+          </div>
+        </div>
+        <div class="card reveal">
+          <div class="card-img">
+            <img src="images/picture 3.jpg" alt="">
+          </div>
+        </div>
+        <div class="card reveal">
+          <div class="card-img">
+            <img src="images/picture 4.jpg" alt="">
+          </div>
+        </div>
+        <div class="card reveal">
+          <div class="card-img">
+            <img src="images/picture 5.jpg" alt="">
+          </div>
+        </div>
+        <div class="card reveal">
+          <div class="card-img">
+            <img src="images/picture 6.jpg" alt="">
+          </div>
+        </div>
+        <div class="title reveal">
+          <a href="#" class="btn">See All</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="contact" id="contact">
+      <div class="title reveal">
+        <h2 class="section-title">Contact Me</h2>
+      </div>
+      <div class="content">
+        <div class="row">
+          <div class="card reveal">
+            <div class="contact-icon">
+              <i class="fas fa-map-marker-alt"></i>
+            </div>
+            <div class="info">
+              <h3>Address</h3>
+              <span>Lagos, Nigeria </span>
+            </div>
+          </div>
+          <div class="card reveal">
+            <div class="contact-icon">
+              <i class="fas fa-phone"></i>
+            </div>
+            <div class="info">
+              <h3>Phone Number</h3>
+              <span>+2349079521011</span>
+            </div>
+          </div>
+          <div class="card reveal">
+            <div class="contact-icon">o
+              <i class="fas fa-envelope"></i>
+            </div>
+            <div class="info">
+              <h3>Email Address</h3>
+   <span>ohussaint@gmail.com@email.com</span>
+            </div>
+          </div>
+            <div class="info">
+          </div>
+        </div>
+        <div class="row">
+          <div class="contact-form reveal">
+            <h3>Send Message</h3>
+            <div class="input-box">
+              <input type="text" name="" value="" placeholder="Name">
+            </div>
+            <div class="input-box">
+              <input type="text" name="" value="" placeholder="Email">
+            </div>
+            <div class="input-box">
+              ,
+            </div>
+            <div class="input-box">
+              <input type="submit" class="send-btn" name="" value="Send"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer">
+      <span class="footer-title">Olayiwola-Frontend-Web Developer Stevens</span>
+      <p>Copyright @2023 <a href="#">@Olayiwola-Frontend-Web Developer</a>. All Rights Reserved.</p>
+    </footer>
+<script src="weather.js" charset="utf-8"></script>
+
+  </body>
+</html>
+
+  
